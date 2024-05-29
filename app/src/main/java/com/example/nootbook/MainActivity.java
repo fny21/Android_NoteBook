b4f67761c353374fc181d5df594e3e45ca818cc8
fny21 <fny21@mails.tsinghua.edu.cn>
2024-05-29 20:38:59 +0800

    listen click event

--- app/src/main/java/com/example/nootbook/MainActivity.java
+++ app/src/main/java/com/example/nootbook/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<String> label_names;
+    private float dp_to_px_ratio;  // 将dp转为像素值时乘的比例因子
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,18 @@ public class MainActivity extends AppCompatActivity {
 
         label_names=new ArrayList<>();
 
-        label_names.add("You have not created a label");
+        label_names.add("Label-hide: You have not created a label, this label is really really long, long long long long long...");
+        label_names.add("Label-show: Again! You have not created a label, this label is really really long, long long long long long...");
+        label_names.add("Note------: 1");
+        label_names.add("Note------: 2");
+        label_names.add("Note------: 3");
+        label_names.add("Note------: 4");
+        label_names.add("Note------: 5");
+        label_names.add("Note------: 6");
+        label_names.add("Note------: 7");
+        label_names.add("Label-hide: Last! You have not created a label, this label is really really long, long long long long long...");
+
+        dp_to_px_ratio = getResources().getDisplayMetrics().density;
 
         // 笔记列表
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.label_list);
@@ -32,9 +46,35 @@ public class MainActivity extends AppCompatActivity {
         //设置为垂直布局，这也是默认的
         layoutManager.setOrientation(RecyclerView.VERTICAL);
         //设置Adapter
-        recyclerView.setAdapter(new labelRecycleViewAdapter(this, this.label_names));
+        labelRecycleViewAdapter label_recycle_view_adapter = new labelRecycleViewAdapter(this, this.label_names, this.dp_to_px_ratio);
+        label_recycle_view_adapter.setOnItemClickListener(new labelRecycleViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, int clicked_item) {
+                String item_name;
+                if(clicked_item==0){
+                    item_name = "background";
+                }
+                else if(clicked_item==1){
+                    item_name = "show_or_hide";
+                }
+                else if(clicked_item==2){
+                    item_name = "name_or_title";
+                }
+                else if(clicked_item==3){
+                    item_name = "add_or_edit";
+                }
+                else if(clicked_item==4){
+                    item_name = "delete";
+                }
+                else{
+                    item_name = "error-unknown" + clicked_item;
+                }
+                Log.e(String.valueOf(this), "position: "+position+", item: "+item_name+" been clicked");
+            }
+        });
+        recyclerView.setAdapter(label_recycle_view_adapter);
         //设置分隔线
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         //设置增加或删除条目的动画
         recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
--- app/src/main/java/com/example/nootbook/labelRecycleViewAdapter.java
+++ app/src/main/java/com/example/nootbook/labelRecycleViewAdapter.java
@@ -1,13 +1,17 @@
 package com.example.nootbook;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -16,33 +20,142 @@ public class labelRecycleViewAdapter extends RecyclerView.Adapter<labelRecycleVi
 
     private Context context;
     private List<String> mList;
+    private String label_prefix;
+    private String label_hide_prefix;
+    private String label_show_prefix;
+    private String note_prefix;
+    private float dp_to_px_ratio;
+
+    // 定义回调接口
+    public interface OnItemClickListener {
+        //clicked_item: 0 for nothing(click the background), 1 for hide or show, 2 for label/text name, 3 for edit/add button, 4 for delete
+        void onItemClick(int position, int clicked_item);
+    }
+
+    // 持有回调接口的引用
+    private OnItemClickListener mListener;
+
+    // 提供设置回调的方法
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
 
     class labelViewHolder extends RecyclerView.ViewHolder {
+        private ImageButton label_show_or_hide;
         private TextView label_name;
-        private List<TextView> Notes;
+        private ImageButton label_add_note;
+        private ImageButton label_delete_all;
 
         public labelViewHolder(@NonNull View itemView, Context context) {
             super(itemView);
             label_name = itemView.findViewById(R.id.one_label_name);
+            label_add_note = itemView.findViewById(R.id.one_label_add_button);
+            label_delete_all = itemView.findViewById(R.id.one_label_delete_all);
+            label_show_or_hide = itemView.findViewById(R.id.label_hide_or_show);
         }
     }
 
-    public labelRecycleViewAdapter(Context context, List<String> mList) {
+    public labelRecycleViewAdapter(Context context, List<String> mList, float dp_to_px_ratio_) {
         this.context=context;
         this.mList = mList;
+        label_prefix = "Label";
+        label_hide_prefix = "Label-hide";
+        label_show_prefix = "Label-show";
+        note_prefix = "Note";
+        dp_to_px_ratio = dp_to_px_ratio_;
     }
 
     @NonNull
     @Override
     public labelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(this.context).inflate(R.layout.label_layout, parent, false);
-        return new labelViewHolder(itemView, this.context);
+        labelViewHolder new_label_view_holder = new labelViewHolder(itemView, this.context);
+
+        // 如果回调接口已设置，可以为ViewHolder中的视图设置点击监听器
+        if (mListener != null) {
+            new_label_view_holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = new_label_view_holder.getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        mListener.onItemClick(position, 0);
+                    }
+                }
+            });
+
+            new_label_view_holder.label_show_or_hide.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = new_label_view_holder.getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        mListener.onItemClick(position, 1);
+                    }
+                }
+            });
+
+            new_label_view_holder.label_name.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = new_label_view_holder.getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        mListener.onItemClick(position, 2);
+                    }
+                }
+            });
+
+            new_label_view_holder.label_add_note.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = new_label_view_holder.getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        mListener.onItemClick(position, 3);
+                    }
+                }
+            });
+
+            new_label_view_holder.label_delete_all.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = new_label_view_holder.getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        mListener.onItemClick(position, 4);
+                    }
+                }
+            });
+        }
+        return new_label_view_holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull labelViewHolder holder, int position) {
         String one_label = this.mList.get(position);
-        holder.label_name.setText(one_label);
+        if(one_label.startsWith(this.label_prefix)){
+            holder.label_add_note.setBackgroundResource(R.drawable.blue_add);
+            holder.label_delete_all.setBackgroundResource(R.drawable.rubbish);
+            if(one_label.startsWith(this.label_hide_prefix)){
+                holder.label_show_or_hide.setBackgroundResource(R.drawable.arrow_label_hide);
+            }
+            else{
+                holder.label_show_or_hide.setBackgroundResource(R.drawable.arrow_label_show);
+            }
+        }
+        else if (one_label.startsWith(this.note_prefix)) {
+            ConstraintLayout.LayoutParams show_or_hide_button_layout = (ConstraintLayout.LayoutParams) holder.label_show_or_hide.getLayoutParams();
+            show_or_hide_button_layout.width = Math.round((float)25 * dp_to_px_ratio);
+            holder.label_show_or_hide.setLayoutParams(show_or_hide_button_layout);
+            holder.label_show_or_hide.setVisibility(View.INVISIBLE);
+            holder.label_add_note.setBackgroundResource(R.drawable.note_edit);
+            holder.label_delete_all.setBackgroundResource(R.drawable.rubbish);
+        }
+        else{
+            Log.e(String.valueOf(this), "unexpected string: string should begin with 'label' or 'note'");
+        }
+        holder.label_name.setText(one_label.substring(12));
+        holder.label_name.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        holder.label_name.setMarqueeRepeatLimit(-1); // -1 表示无限重复
+        holder.label_name.setFocusable(true);
+        holder.label_name.setFocusableInTouchMode(true);
+        holder.label_name.setSelected(true);
     }
 
     @Override
